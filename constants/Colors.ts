cf055c9ab470dfdd63c5ba53230a891a52f8d0a0
Salay Abdul Muhaimin Kanton <abdulkanton2005@gmail.com>
2025-01-08 10:55:34 +0000

    Update Colours

--- constants/Colors.ts
+++ constants/Colors.ts
@@ -1,11 +1,14 @@
 const Colors = {
-    primary: "#4CAF50",
+    bg: "#f0fdf4",
+    bg2: "#dcfce7",
+    primary: "#80C011",
+    primary2: "#6CA20E",
     secondary: "#FFC107",
     background: "#FFFFFF",
     text: "#212121",
     error: "#F44336",
     success: "#4CAF50",
-    warning: "#FF9800",
+    warning: "#FF9800"
 };
 
 export default Colors;
